f17cfa917bf722569c3858690a913d0ea42eb1d0
Matthew Zdanevich <mzdanevich@outlook.com>
2024-03-02 05:39:46 +0300

    Solve a mistake

--- sandbox.cpp
+++ sandbox.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::string name;
 	std::cout << "Enter your name: ";
 	std::cin >> name;
-	std::cout << "Hello " << name << " !";
+	std::cout << "Hello, " << name << " !";
 	
 	return 0;
 }
