71559d3372b9fafad46f0777b1c3bc0df5848438
Matthew Zdanevich <mzdanevich@outlook.com>
2024-03-02 04:53:08 +0300

    Hello Git and your name

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
 
-int main() { 
+int main() {
 	std::cout << "Hello Git!" << std::endl;
+	
+	std::string name;
+	std::cout << "Enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello " << name << " !";
+	
 	return 0;
 }
